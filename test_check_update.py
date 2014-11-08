458aee568bc59aaa95924b002ed79467141bea56
Andrew Ziem <ahz001@gmail.com>
2014-11-07 20:27:41 -0700

    Automatic check of versions returned by online update

---
+++ test_check_update.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# vim: ts=4:sw=4:expandtab
+
+# Copyright (C) 2014 by Andrew Ziem.  All rights reserved.
+# License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+#
+#
+
+"""
+Verify the online update notification system returns the right version numbers
+"""
+
+import os
+import sys
+dir_bb_root = os.path.abspath('../bleachbit')
+os.chdir(dir_bb_root)
+sys.path.append(dir_bb_root)
+import bleachbit.Update
+import bleachbit.Common
+
+# tuple in the format
+# (current version sent, version returned 1, version returned 2)
+tests = \
+    (('1.2', '1.4', None),
+     ('1.4', '1.5.2 (beta)', None),
+     ('1.5.1', '1.5.2 (beta)', None),
+     ('1.5.2', None, None))
+
+for test in tests:
+    print '\n', '*' * 10, test[0]
+    bleachbit.Common.APP_VERSION = test[0]
+    v1e = test[1]  # e=expected
+    v2e = test[2]
+    cu = bleachbit.Update.check_updates(True, False, None, None)
+    print 'returned=', cu
+    match = False
+    if cu == ():
+        v1r = None  # r=returned
+        v2r = None
+    else:
+        if cu[0]:
+            v1r = cu[0][0]
+        else:
+            v1r = None
+        if len(cu) > 1:
+            v2r = cu[1][0]
+        else:
+            v2r = None
+    if not v1e == v1r:
+        print 'ERROR: sent version %s, expected v1=%s, returned v1=%s' % \
+            (test[0], v1e, v1r)
+    if not v2e == v2r:
+        print 'ERROR: sent version %s, expected v1=%s, returned v2=%s' % \
+            (test[0], v2e, v2r)
