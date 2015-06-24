ecb645ca5fd16a8e1db100840b8016dfd80a9e38
Andrew Ziem <ahz001@gmail.com>
2015-06-23 21:15:08 -0600

    autopep8

--- build_web_download.py
+++ build_web_download.py
@@ -36,7 +36,8 @@ def url_to_distro(url):
         (distro, ver) = d.split("_")
     if "xUbuntu" == distro:
         distro = "Ubuntu"
-    return (distro,ver)
+    return (distro, ver)
+
 
 def make_tag(distro, ver):
 
@@ -80,81 +81,82 @@ def make_tag(distro, ver):
         return 'ubuntu' + ver
     raise Exception("Unknown distro %s" % (distro,))
 
+
 def filename_to_distro(filename):
     if filename.find('centosCentOS-6') > -1:
-    #bleachbit-0.9.0beta-1.1.centosCentOS-6.noarch.rpm
+    # bleachbit-0.9.0beta-1.1.centosCentOS-6.noarch.rpm
         return 'CentOS 6'
     tag = re.findall("\.([a-z]*[0-9]*)\.noarch.rpm$", filename)
     if 1 == len(tag):
-        distros = { \
-            'centos5' : 'CentOS 5', \
-            'centos6' : 'CentOS 6', \
-            'centos7' : 'CentOS 7', \
-            'fc8' : 'Fedora 8 (Werewolf)', \
-            'fc9' : 'Fedora 9 (Sulphur)', \
-            'fc10' : 'Fedora 10 (Cambridge)', \
-            'fc11' : 'Fedora 11 (Leonidas)', \
-            'fc12' : 'Fedora 12 (Constantine)', \
-            'fc13' : 'Fedora 13 (Goddard)', \
-            'fc14' : 'Fedora 14 (Laughlin)', \
-            'fc15' : 'Fedora 15 (Lovelock)', \
-            'fc16' : 'Fedora 16 (Verne)', \
-            'fc17' : 'Fedora 17 (Beefy Miracle)', \
-            'fc18' : 'Fedora 18 (Spherical Cow)', \
-            'fc19' : 'Fedora 19 (Schrodinger\'s Cat)', \
-            'fc20' : 'Fedora 20 (Heisenbug)', \
-            'fc21' : 'Fedora 21', \
-            'mdv2008' : 'Mandriva 2008', \
-            'mdv2009' : 'Mandriva 2009', \
-            'mdv20091' : 'Mandriva 2009.1', \
-            'mdv2010' : 'Mandriva 2010', \
-            'mdv20101' : 'Mandriva 2010.1', \
-            'opensuse103' : 'openSUSE 10.3', \
-            'opensuse110' : 'openSUSE 11.0', \
-            'opensuse111' : 'openSUSE 11.1', \
-            'opensuse112' : 'openSUSE 11.2', \
-            'opensuse113' : 'openSUSE 11.3', \
-            'opensuse114' : 'openSUSE 11.4', \
-            'opensuse121' : 'openSUSE 12.1', \
-            'opensuse122' : 'openSUSE 12.2', \
-            'opensuse123' : 'openSUSE 12.3', \
-            'opensuse131' : 'openSUSE 13.1', \
-            'opensuse132' : 'openSUSE 13.2', \
-            'el4' : '<acronym title="Red Hat Enterprise Linux">RHEL</acronym> 4', \
-            'el5' : 'RHEL 5', \
-            'el6' : 'RHEL 6', \
-            'el7' : 'RHEL 7', \
-            'sles9' : '<acronym title="SUSE Linux Enterprise Server">SLES</acronym> 9', \
-            'sle10' : '<acronym title="SUSE Linux Enterprise">SLE</acronym> 10',
-            'sle11' : '<acronym title="SUSE Linux Enterprise">SLE</acronym> 11' \
-            }
+        distros = {
+            'centos5': 'CentOS 5',
+            'centos6': 'CentOS 6',
+            'centos7': 'CentOS 7',
+            'fc8': 'Fedora 8 (Werewolf)',
+            'fc9': 'Fedora 9 (Sulphur)',
+            'fc10': 'Fedora 10 (Cambridge)',
+            'fc11': 'Fedora 11 (Leonidas)',
+            'fc12': 'Fedora 12 (Constantine)',
+            'fc13': 'Fedora 13 (Goddard)',
+            'fc14': 'Fedora 14 (Laughlin)',
+            'fc15': 'Fedora 15 (Lovelock)',
+            'fc16': 'Fedora 16 (Verne)',
+            'fc17': 'Fedora 17 (Beefy Miracle)',
+            'fc18': 'Fedora 18 (Spherical Cow)',
+            'fc19': 'Fedora 19 (Schrodinger\'s Cat)',
+            'fc20': 'Fedora 20 (Heisenbug)',
+            'fc21': 'Fedora 21',
+            'mdv2008': 'Mandriva 2008',
+            'mdv2009': 'Mandriva 2009',
+            'mdv20091': 'Mandriva 2009.1',
+            'mdv2010': 'Mandriva 2010',
+            'mdv20101': 'Mandriva 2010.1',
+            'opensuse103': 'openSUSE 10.3',
+            'opensuse110': 'openSUSE 11.0',
+            'opensuse111': 'openSUSE 11.1',
+            'opensuse112': 'openSUSE 11.2',
+            'opensuse113': 'openSUSE 11.3',
+            'opensuse114': 'openSUSE 11.4',
+            'opensuse121': 'openSUSE 12.1',
+            'opensuse122': 'openSUSE 12.2',
+            'opensuse123': 'openSUSE 12.3',
+            'opensuse131': 'openSUSE 13.1',
+            'opensuse132': 'openSUSE 13.2',
+            'el4': '<acronym title="Red Hat Enterprise Linux">RHEL</acronym> 4',
+            'el5': 'RHEL 5',
+            'el6': 'RHEL 6',
+            'el7': 'RHEL 7',
+            'sles9': '<acronym title="SUSE Linux Enterprise Server">SLES</acronym> 9',
+            'sle10': '<acronym title="SUSE Linux Enterprise">SLE</acronym> 10',
+            'sle11': '<acronym title="SUSE Linux Enterprise">SLE</acronym> 11'
+        }
         return distros[tag[0]]
     tag = re.findall("_([a-z]*[0-9]*)\.deb$", filename)
     if 1 == len(tag):
-        distros = { \
-            'ubuntu606' : 'Ubuntu 6.06 LTS (Dapper Drake)', \
-            'ubuntu710' : 'Ubuntu 7.10 (Feisty Fawn)', \
-            'ubuntu804' : 'Ubuntu 8.04 LTS (Hardy Heron)', \
-            'ubuntu810' : 'Ubuntu 8.10 (Intrepid Ibex)', \
-            'ubuntu904' : 'Ubuntu 9.04 (Jaunty Jackalope)', \
-            'ubuntu910' : 'Ubuntu 9.10 (Karmic Koala)', \
-            'ubuntu1004' : 'Ubuntu 10.04 LTS (Lucid Lynx)', \
-            'ubuntu1010' : 'Ubuntu 10.10 (Maverick Meerkat)', \
-            'ubuntu1104' : 'Ubuntu 11.04 (Natty Narwhal)', \
-            'ubuntu1110' : 'Ubuntu 11.10 (Oneiric Ocelot)', \
-            'ubuntu1204' : 'Ubuntu 12.04 (Precise Pangolin)', \
-            'ubuntu1210' : 'Ubuntu 12.10 (Quantal Quetzal)', \
-            'ubuntu1304' : 'Ubuntu 13.04 (Raring Ringtail)', \
-            'ubuntu1310' : 'Ubuntu 13.10 (Saucy Salamander)', \
-            'ubuntu1404' : 'Ubuntu 14.04 LTS (Trusty Tahr)', \
-            'ubuntu1410' : 'Ubuntu 14.10 (Utopic Unicorn)', \
-            'ubuntu1504' : 'Ubuntu 15.04 (Vivid Vervet)', \
-            'debian4' : 'Debian 4 (Etch)', \
-            'debian5' : 'Debian 5 (Lenny)', \
-            'debian6' : 'Debian 6 (Squeeze)', \
-            'debian7' : 'Debian 7 (Wheezy)',
-            'debian8' : 'Debian 8 (Jessie)' \
-            }
+        distros = {
+            'ubuntu606': 'Ubuntu 6.06 LTS (Dapper Drake)',
+            'ubuntu710': 'Ubuntu 7.10 (Feisty Fawn)',
+            'ubuntu804': 'Ubuntu 8.04 LTS (Hardy Heron)',
+            'ubuntu810': 'Ubuntu 8.10 (Intrepid Ibex)',
+            'ubuntu904': 'Ubuntu 9.04 (Jaunty Jackalope)',
+            'ubuntu910': 'Ubuntu 9.10 (Karmic Koala)',
+            'ubuntu1004': 'Ubuntu 10.04 LTS (Lucid Lynx)',
+            'ubuntu1010': 'Ubuntu 10.10 (Maverick Meerkat)',
+            'ubuntu1104': 'Ubuntu 11.04 (Natty Narwhal)',
+            'ubuntu1110': 'Ubuntu 11.10 (Oneiric Ocelot)',
+            'ubuntu1204': 'Ubuntu 12.04 (Precise Pangolin)',
+            'ubuntu1210': 'Ubuntu 12.10 (Quantal Quetzal)',
+            'ubuntu1304': 'Ubuntu 13.04 (Raring Ringtail)',
+            'ubuntu1310': 'Ubuntu 13.10 (Saucy Salamander)',
+            'ubuntu1404': 'Ubuntu 14.04 LTS (Trusty Tahr)',
+            'ubuntu1410': 'Ubuntu 14.10 (Utopic Unicorn)',
+            'ubuntu1504': 'Ubuntu 15.04 (Vivid Vervet)',
+            'debian4': 'Debian 4 (Etch)',
+            'debian5': 'Debian 5 (Lenny)',
+            'debian6': 'Debian 6 (Squeeze)',
+            'debian7': 'Debian 7 (Wheezy)',
+            'debian8': 'Debian 8 (Jessie)'
+        }
         return distros[tag[0]]
 
     if filename.endswith('.exe'):
@@ -163,18 +165,19 @@ def filename_to_distro(filename):
     raise Exception("unknown distro for '%s'" % filename)
 
 
-
 def url_to_filename(url):
-    (distro,ver) = url_to_distro(url)
+    (distro, ver) = url_to_distro(url)
     try:
         tag = make_tag(distro, ver)
     except:
         traceback.print_exc()
         sys.stderr.flush()
-        sys.stderr.write("url = '%s', distro = '%s', ver=%s\n" % (url, distro, ver))
+        sys.stderr.write(
+            "url = '%s', distro = '%s', ver=%s\n" % (url, distro, ver))
         raise
-    #print "distro = '%s', ver = '%s', tag = '%s', url = '%s'" % (distro, ver, tag, url[57:])
-    old_fn = url[url.rfind("/") + 1 : ]
+    # print "distro = '%s', ver = '%s', tag = '%s', url = '%s'" % (distro,
+    # ver, tag, url[57:])
+    old_fn = url[url.rfind("/") + 1:]
     if 0 <= old_fn.find("noarch"):
         return old_fn.split(".noarch")[0] + "." + tag + ".noarch.rpm"
     if old_fn.endswith(".deb"):
@@ -187,12 +190,12 @@ def process_url(url):
     print ("wget -nv -nc -O %s %s" % (fn, url))
 
 
-
 def get_repo_urls(osc_dir):
     print "* Getting list of URLs"
     old_cwd = os.getcwd()
     os.chdir(osc_dir)
-    repourls = subprocess.Popen(["osc", "repourls"], stdout=subprocess.PIPE).communicate()[0]
+    repourls = subprocess.Popen(
+        ["osc", "repourls"], stdout=subprocess.PIPE).communicate()[0]
     repourls = repourls.split("\n")
     os.chdir(old_cwd)
     return repourls
@@ -206,24 +209,25 @@ def get_files_in_repo_sub(url):
         print str(sys.exc_info()[1])
         return []
     files = []
-    for (file,ext) in re.findall('"([a-z0-9_.-]*)(rpm|deb)"', dir):
+    for (file, ext) in re.findall('"([a-z0-9_.-]*)(rpm|deb)"', dir):
         fn = file + ext
         fileurl = url + fn
         print "found fileurl '%s'" % (fileurl,)
         files.append(fileurl)
     return files
 
+
 def get_files_in_repo(repourl):
     # strip off the filename
     pos = repourl.rfind("/")
-    baseurl = repourl[0:pos+1]
+    baseurl = repourl[0:pos + 1]
     if 0 <= repourl.find("buntu") or 0 <= repourl.find("ebian"):
-        #files = []
-        #for file in get_files_in_repo_sub(baseurl + "i386/"):
+        # files = []
+        # for file in get_files_in_repo_sub(baseurl + "i386/"):
         #    files.append(file)
-        #for file in get_files_in_repo_sub(baseurl + "amd64/"):
+        # for file in get_files_in_repo_sub(baseurl + "amd64/"):
         #    files.append(file)
-        #return files
+        # return files
         return get_files_in_repo_sub(baseurl + "all/")
     else:
         return get_files_in_repo_sub(baseurl + "noarch/")
@@ -239,25 +243,28 @@ def get_files_in_repos(repourls):
     print files
     return files
 
+
 def distro_to_target(distro):
     if distro.startswith("Fedora") or distro.startswith("Cent") or distro.startswith("RHEL"):
         return "/etc/yum.repos.d/"
     return ""
 
+
 def strip_tags(value):
     """Return the given HTML with all tags stripped."""
-    #http://smitbones.blogspot.com/2008/01/python-strip-html-tags-function.html
-    return re.sub(r'<[^>]*?>', '', value) 
+    # http://smitbones.blogspot.com/2008/01/python-strip-html-tags-function.html
+    return re.sub(r'<[^>]*?>', '', value)
+
 
 def create_html_snippet(filenames, header):
     print "* Creating HTML snippet"
-    f = open ("snippet_%s.html" % (header.lower().replace(" ","_"),), "w")
+    f = open("snippet_%s.html" % (header.lower().replace(" ", "_"),), "w")
     f.write("<ul>\n")
     target = 0 <= header.find("epo")
     records = []
     for filename in filenames:
         if len(filename) < 5 \
-            or re.search('(bonus|(tar.(bz2|lzma|gz)|zip|txt|txt.asc|html)$)', filename):
+                or re.search('(bonus|(tar.(bz2|lzma|gz)|zip|txt|txt.asc|html)$)', filename):
             continue
         distro = filename_to_distro(filename)
         # this url works as of 9/14/2010
@@ -274,16 +281,18 @@ def create_html_snippet(filenames, header):
 #    print records
 
     for (distro_txt, distro, url, filename) in records:
-        f.write("<li><a rel=\"external nofollow\" href=\"%(url)s\">%(distro)s</a></li>\n" % \
-            { 'url' : url, 'distro' : distro })
+        f.write("<li><a rel=\"external nofollow\" href=\"%(url)s\">%(distro)s</a></li>\n" %
+                {'url': url, 'distro': distro})
 
     f.write("</ul>\n")
 
+
 def dump_file_urls(fileurls):
     print "* Dumping file URLs"
-    f = open ("file_urls.txt", "w")
+    f = open("file_urls.txt", "w")
     for url in fileurls:
         f.write("%s\n" % (url,))
+
 
 def main():
     import sys
@@ -299,7 +308,7 @@ def main():
         filenames = sorted(os.listdir('files'))
         create_html_snippet(filenames, "Installation package")
     else:
-        raise RuntimeError('Unknown command line'+sys.argv[1])
+        raise RuntimeError('Unknown command line' + sys.argv[1])
 
 
 if __name__ == '__main__':
