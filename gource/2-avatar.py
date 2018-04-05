646f9733dc1f1aa5510011708b8d2f6040bf71a9
Andrew Ziem <ahz001@gmail.com>
2018-04-04 21:59:16 -0600

    autopep

--- gource/2-avatar.py
+++ gource/2-avatar.py
@@ -44,108 +44,108 @@ committer_filename = os.path.join(work_dir, 'committer2')
 
 
 def md5_hex(text):
-  m = hashlib.md5()
-  m.update(text.encode('ascii', errors='ignore'))
-  return m.hexdigest()
+    m = hashlib.md5()
+    m.update(text.encode('ascii', errors='ignore'))
+    return m.hexdigest()
 
 
 def get_data(api_request):
-  global username
-  global password
+    global username
+    global password
 
-  r = requests.get(api_request, auth=(username, password))
-  data = r.json()
-
-  if "message" in data.keys():
-    print data['message']
-    if data['message'] == 'Must specify two-factor authentication OTP code.':
-        sys.exit(1)
-    # Countdown
-    # http://stackoverflow.com/questions/3249524/print-in-one-line-dynamically-python
-    for k in range(1, 60 * 15):
-      remaining = 60 * 15 - k
-      sys.stdout.write("\r%d seconds remaining   " % remaining)
-      sys.stdout.flush()
-      sleep(1)
-    sys.stdout.write("\n")
-    # Another request
     r = requests.get(api_request, auth=(username, password))
     data = r.json()
-  else:
-    pass
 
-  # Return data
-  return data
+    if "message" in data.keys():
+        print data['message']
+        if data['message'] == 'Must specify two-factor authentication OTP code.':
+            sys.exit(1)
+        # Countdown
+        # http://stackoverflow.com/questions/3249524/print-in-one-line-dynamically-python
+        for k in range(1, 60 * 15):
+            remaining = 60 * 15 - k
+            sys.stdout.write("\r%d seconds remaining   " % remaining)
+            sys.stdout.flush()
+            sleep(1)
+        sys.stdout.write("\n")
+        # Another request
+        r = requests.get(api_request, auth=(username, password))
+        data = r.json()
+    else:
+        pass
+
+    # Return data
+    return data
 
 if __name__ == "__main__":
-  print 'starting'
-  global username
-  global password
+    print 'starting'
+    global username
+    global password
 
-  # Clear screen
-  #os.system('cls' if os.name == 'nt' else 'clear')
+    # Clear screen
+    #os.system('cls' if os.name == 'nt' else 'clear')
 
-  # Login to the GitHub API
-  username = raw_input("Enter your GitHub username: ")
-  password = getpass.getpass("Enter your GitHub password: ")
+    # Login to the GitHub API
+    username = raw_input("Enter your GitHub username: ")
+    password = getpass.getpass("Enter your GitHub password: ")
 
-  # Create the folder for storing the images. It's in the .git folder, so it won't be tracked by git
-  output_dir = os.path.expanduser(avatar_dir)
-  if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+    # Create the folder for storing the images. It's in the .git folder, so it won't be tracked by git
+    output_dir = os.path.expanduser(avatar_dir)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-  # Get the authors from the Git log
-  committer_filename = os.path.expanduser(log_path)
-  print 'Committer filename', committer_filename
-  authors = []
-  with open(committer_filename) as cf:
-    for line in cf:
-        authors.append(line.replace('\n',''))
-  print ""
-  print "USERS:"
-  print(authors)
-
-  # Check each author
-  for author in authors:
-    # Get e-mail and name from log
-    email, name = author.split('|')
+    # Get the authors from the Git log
+    committer_filename = os.path.expanduser(log_path)
+    print 'Committer filename', committer_filename
+    authors = []
+    with open(committer_filename) as cf:
+        for line in cf:
+            authors.append(line.replace('\n', ''))
     print ""
-    print "Checking", name, email
-    # Try to find the user on GitHub with the e-mail
-    api_request = "https://api.github.com/search/users?utf8=%E2%9C%93&q=" + \
-        email + "+in%3Aemail&type=Users"
-    data = get_data(api_request)
+    print "USERS:"
+    print(authors)
 
-    # Check if the user was found
-    if "items" in data.keys():
-      if len(data["items"]) == 1:
-        url = data["items"][0]["avatar_url"]
-        print "Avatar url:", url
-      else:
-        # Try to find the user on GitHub with the name
+    # Check each author
+    for author in authors:
+        # Get e-mail and name from log
+        email, name = author.split('|')
+        print ""
+        print "Checking", name, email
+        # Try to find the user on GitHub with the e-mail
         api_request = "https://api.github.com/search/users?utf8=%E2%9C%93&q=" + \
-            name + "+in%3Aname&type=Users"
+            email + "+in%3Aemail&type=Users"
         data = get_data(api_request)
 
         # Check if the user was found
         if "items" in data.keys():
-          if len(data["items"]) == 1:
-            url = data["items"][0]["avatar_url"]
-            print "Avatar url:", url
-          # Eventually try to find the user with Gravatar
-          else:
-            # d=404 returns no file if user not found
-            url = "http://www.gravatar.com/avatar/" + \
-                md5_hex(email) + "?d=404&s=" + str(90)
-            print "Avatar url:", url
+            if len(data["items"]) == 1:
+                url = data["items"][0]["avatar_url"]
+                print "Avatar url:", url
+            else:
+                # Try to find the user on GitHub with the name
+                api_request = "https://api.github.com/search/users?utf8=%E2%9C%93&q=" + \
+                    name + "+in%3Aname&type=Users"
+                data = get_data(api_request)
 
-    # Finally retrieve the image
-    try:
-      output_file = os.path.join(output_dir, name + '.png')
-      if not os.path.exists(output_file):
-        r = requests.get(url)
-        if r.ok:
-          with open(output_file, 'wb') as img:
-            img.write(r.content)
-    except:
-      print "There was an error with", name, email
+                # Check if the user was found
+                if "items" in data.keys():
+                    if len(data["items"]) == 1:
+                        url = data["items"][0]["avatar_url"]
+                        print "Avatar url:", url
+                    # Eventually try to find the user with Gravatar
+                    else:
+                        # d=404 returns no file if user not found
+                        url = "http://www.gravatar.com/avatar/" + \
+                            md5_hex(email) + "?d=404&s=" + str(90)
+                        print "Avatar url:", url
+
+        # Finally retrieve the image
+        try:
+            output_file = os.path.join(output_dir, name + '.png')
+            if not os.path.exists(output_file):
+                r = requests.get(url)
+                if r.ok:
+                    with open(output_file, 'wb') as img:
+                        img.write(r.content)
+        except:
+            print "There was an error with", name, email
