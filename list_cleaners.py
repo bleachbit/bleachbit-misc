033d49d2a69a33ed190cf52cc50d61525237afda
Andrew Ziem <ahz001@gmail.com>
2019-03-18 22:17:06 -0600

    Update for Git path
    No longer SVN

--- list_cleaners.py
+++ list_cleaners.py
@@ -2,7 +2,7 @@
 # vim: ts=4:sw=4:expandtab
 
 
-# Copyright (C) 2009 by Andrew Ziem.  All rights reserved.
+# Copyright (C) 2009-2019 by Andrew Ziem.  All rights reserved.
 # License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
 # This is free software: you are free to change and redistribute it.
 # There is NO WARRANTY, to the extent permitted by law.
@@ -17,16 +17,16 @@ import gettext
 import os
 import sys
 
-os.chdir('../trunk')
+os.chdir('../bleachbit')
 sys.path.append(".")
 
-import bleachbit.Common
 from bleachbit.Cleaner import backends
 from bleachbit.CleanerML import load_cleaners
 
 
 def main():
-    bleachbit.Common.personal_cleaners_dir = '../bonus/cleaners'
+    # This could be used with the CleanerML repository
+    #bleachbit.Common.personal_cleaners_dir = '../cleanerml'
     load_cleaners()
     for key in sorted(backends):
 
