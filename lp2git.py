43223e02c43f6220cf2fe64cdb364e189596734f
Andrew Ziem <ahz001@gmail.com>
2019-05-31 10:50:11 -0600

    Update for Python 3
    - Add code comments
    - Update to Beautiful Soup 4
    - Fix: standardize the standardization of msgid (regarding line break and white space)

--- lp2git.py
+++ lp2git.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # vim: ts=4:sw=4:expandtab
 
-# Copyright (C) 2014 by Andrew Ziem.  All rights reserved.
+# Copyright (C) 2014-2019 by Andrew Ziem.  All rights reserved.
 # License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
 # This is free software: you are free to change and redistribute it.
 # There is NO WARRANTY, to the extent permitted by law.
@@ -11,13 +11,12 @@
 
 """Import translated .po files from Launchpad to Git"""
 
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 from polib import pofile
-from urllib import urlencode
+from urllib.parse import urlencode
 import os
 import re
 import sys
-import HTMLParser
 
 
 # example : translations['es']['Preview'] =
@@ -26,40 +25,64 @@ translations = {}
 
 
 def read_http(url):
-    import urllib2
-    opener = urllib2.build_opener()
+    from urllib.request import build_opener
+    opener = build_opener()
     opener.addheaders = [('User-agent', 'lp2git')]
-    return opener.open(url).read()
+    return opener.open(url).read().decode()
 
 
 def parse_search_html(lang_id, msgctxt, msgid, start):
+    """
+    Query Launchpad for a message, and add its URL to the
+    global dictionary.
+
+    Parameters
+    ----------
+    lang_id: language code such as en_GB
+    msgctxt: message context
+    msgid: message (i.e.g, English string)
+    start: index for pagination
+
+    Returns
+    -------
+    integer: value of start indicating to get more results
+    None: there are no more search results
+    """
     param = urlencode({'show': 'all',
                        'search': msgid,
                        'start': start})
     url = 'https://translations.launchpad.net/bleachbit/trunk/+pots/bleachbit/%s/+translate?%s' \
         % (lang_id, param)
-    print 'debug: fetch url %s ' % url
+    print ('debug: fetch url %s ' % url)
     doc = read_http(url)
-    soup = BeautifulSoup(doc)
-    h = HTMLParser.HTMLParser()
+    soup = BeautifulSoup(doc, features="lxml")
     for tr in soup.findAll('tr', attrs={'class': 'translation'}):
+        # en_div: div element that contains the English translation
         en_div = tr.find('div', attrs={'id': re.compile("^msgset_[0-9]+_")})
-        en_txt = h.unescape(en_div.text)
-                            # unescape for example for "environment&#x27;s"
-        en_div_id = [x[1] for x in en_div.attrs if x[0] == 'id'][0]
+        if not en_div:
+            continue
+        from html import unescape
+        # en_txt: English version of the message (i.e., untranslated)
+        # unescape for example for "environment&#x27;s"
+        en_txt = unescape(en_div.text)
+        en_div_id = en_div.attrs['id']
         en_ctxt_div = soup.findAll(
             id=en_div_id.replace('singular', 'context'))
         if en_ctxt_div:
+            # Specific (i.e., non-default) context of the message.
             en_ctxt = en_ctxt_div[0].text
         else:
+            # Default context. This is the most common.
             en_ctxt = "none"
-        for attr in tr.find('a').attrs:
-            if attr[0] == 'href':
-                href = attr[1]
+        if not tr.find('a'):
+            continue
+        # href: link to page with just one message
+        href = tr.find('a').attrs['href']
         if not en_ctxt in translations[lang_id].keys():
             # initialize the context
             translations[lang_id][en_ctxt] = {}
         translations[lang_id][en_ctxt][en_txt] = href
+        del href
     more = soup.findAll('a', attrs={'class': 'next'})
     if more:
         # more results
@@ -72,9 +95,9 @@ def parse_search_html(lang_id, msgctxt, msgid, start):
 
 def parse_detail_html(url):
     """Parse a Launchpad page for an individual translation message"""
-    print 'debug: fetch url %s ' % url
+    print ('debug: fetch url %s ' % url)
     doc = read_http(url)
-    soup = BeautifulSoup(doc)
+    soup = BeautifulSoup(doc, features="lxml")
     label = soup.find('label', 'no-translation')
     if label:
         raise RuntimeError('not translated')
@@ -103,11 +126,10 @@ def who_translated(lang_id, msgctxt, msgid):
     start = 0
     while True:
         start = parse_search_html(lang_id, msgctxt, msgid, start)
-        msgid2 = msgid.replace('\n', '').rstrip()
         msgctxt_key = "none" if msgctxt is None else msgctxt
         if msgctxt_key in translations[lang_id]:
-            if msgid2 in translations[lang_id][msgctxt_key]:
-                url = translations[lang_id][msgctxt_key][msgid2]
+            if msgid in translations[lang_id][msgctxt_key]:
+                url = translations[lang_id][msgctxt_key][msgid]
                 return parse_detail_html(url)
         if None == start:
             raise RuntimeError('not found "%s"' % msgid)
@@ -133,6 +155,7 @@ def process_po(lang_id):
     po_new = pofile(new_fn)
     po_old = pofile(old_fn)
     msgids = []
+    # Build list of new messages
     for new_entry in po_new.translated_entries():
         msgids.append([new_entry.msgctxt, new_entry.msgid])
         for old_entry in po_old.translated_entries():
@@ -145,14 +168,16 @@ def process_po(lang_id):
                 msgids.pop()
                 break
 
+    print('Count of new messages:', len(msgids))
+
     if not msgids:
-        print 'No changes for language %s' % lang_id
+        print ('No changes for language %s' % lang_id)
         return None
 
     names = []
     for (msgctxt, msgid) in msgids:
-        print "looking for msgctxt '%s' msgid '%s' for lang '%s'" % \
-            (msgctxt, msgid, lang_id)
+        print ("looking for msgctxt '%s' msgid '%s' for lang '%s'" % \
+            (msgctxt, msgid, lang_id))
         names = names + who_translated(lang_id, msgctxt, msgid)
     lang_name = get_lang_name(po_new, lang_id)
     if len(set(names)) == 1:
@@ -175,23 +200,27 @@ def download_po_files(urls):
             raise RuntimeError('file %s.po does not exist' % lang_id)
 
     for url in urls:
-        print 'debug: downloading url %s' % url
+        print ('debug: downloading url %s' % url)
         doc = read_http(url)
         ret = re.search('-([a-z]{2,3}(_[A-Z]{2})?).po$', url, re.I)
         lang_id = ret.groups(0)[0]
-        f = file(lang_id + '_new.po', 'w')
-        f.write(doc)
-        f.close()
+        with open(lang_id + '_new.po', 'w') as f:
+            f.write(doc)
         cmd = process_po(lang_id)
         if cmd:
             langs[lang_id] = cmd
 
-    for lang_id, cmd in langs.iteritems():
-        print 'mv %s_new.po %s.po' % (lang_id, lang_id)
+    for lang_id, cmd in langs.items():
+        print ('mv %s_new.po %s.po' % (lang_id, lang_id))
         os.rename('%s_new.po' % lang_id, '%s.po' % lang_id)
-        print cmd
+        print (cmd)
         # cmd is a Unicode, so encode to bytestring to avoid
         # UnicodeEncodeError: 'ascii' codec can't encode character u'\xf6' in position 49: ordinal not in range(128)
         os.system(cmd.encode('utf-8'))
 
-download_po_files(sys.argv[1:])
+def go():
+    """Main program"""
+    download_po_files(sys.argv[1:])
+
+if __name__ == '__main__':
+    go()
