af68e2063ef5c8351e6700875fc8db80c559e7fc
Andrew Ziem <ahz001@gmail.com>
2021-11-13 14:41:52 -0700

    Update versions in update checker

--- test_check_update.py
+++ test_check_update.py
@@ -22,7 +22,7 @@ sys.path.append(dir_bb_root)
 import bleachbit
 import bleachbit.Update
 
-latest_stable = '4.4.0'
+latest_stable = '4.4.2'
 latest_beta = None
 
 # tuple in the format
@@ -39,7 +39,9 @@ tests = \
      ('4.1.0', latest_stable, None),
      ('4.1.1', latest_stable, None),
      ('4.2.0', latest_stable, None),
-     ('4.4.0', None, None))
+     ('4.4.0', latest_stable, None),
+     ('4.4.1', latest_stable, None),
+     ('4.4.2', None, None))
 
 for test in tests:
     print('\n', '*' * 10, test[0])
