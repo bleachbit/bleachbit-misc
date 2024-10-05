f50bc80cda071dd0b4e457caa87c8a39e723a018
Andrew Ziem <ahz001@gmail.com>
2024-10-05 14:44:51 -0600

    Automatically make language files
    * Update code comments
    * Improve hint
    * Remove unused import

--- extract_desktop.py
+++ extract_desktop.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # vim: ts=4:sw=4:expandtab
 
-# Copyright (C) 2014 by Andrew Ziem.  All rights reserved.
+# Copyright (C) 2014-2024 by Andrew Ziem.  All rights reserved.
 # License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
 # This is free software: you are free to change and redistribute it.
 # There is NO WARRANTY, to the extent permitted by law.
@@ -14,9 +14,9 @@ This program helps manage the translations in the bleachbit.desktop file.
 
 Here is how it works
 1. The bleachbit/po/Makefile adds strings from bleachbit.desktop to bleachbit.pot
-2. I upload bleachbit.pot to Launchpad
-3. Launchpad asks users to translate the strings
-4. I download the .po files
+2. Weblate syncs translation strings from GitHub to hosted.weblate.org
+3. Weblate asks users to translate the strings
+4. Weblate pushes the .po files to the GitHub repository
 5. This program extracts the two strings
 6. I copy the output of this program and paste it into bleachbit.desktop
 """
@@ -25,8 +25,6 @@ Here is how it works
 import gettext
 import os
 import sys
-import subprocess
-import re
 
 bleachbit_repo_dir = '../bleachbit'
 if not os.path.exists(bleachbit_repo_dir):
@@ -67,11 +65,19 @@ def print_desktop_keys(key, value):
 def main():
     if not os.path.exists('po/es.po'):
         print('ERROR: po/es.po does not exist. It should always be in the bleachbit repo.')
+        print('Tip: Verify you are in the right directory.')
         sys.exit(1)
     if not os.path.exists('po/es.mo'):
         print('ERROR: po/es.mo does not exist, so it seems translations are not compiled.')
-        print('Tip: try running "make -c po local" from the bleachbit repo.')
-        sys.exit(1)
+        print('Tip: try running "make -C po local" from the bleachbit repo.')
+        print('Running the command for you.')
+        ret = subprocess.run(['make', '-C', 'po', 'local'])
+        if ret.returncode != 0:
+            print('ERROR: running "make -C po local" failed')
+            sys.exit(1)
+        if not os.path.exists('po/es.mo'):
+            print('ERROR: po/es.mo does not exist even after running "make -C po local"')
+            sys.exit(1)
     print_desktop_keys('Comment', 'Free space and maintain privacy')
     print_desktop_keys('GenericName', 'Unnecessary file cleaner')
 
